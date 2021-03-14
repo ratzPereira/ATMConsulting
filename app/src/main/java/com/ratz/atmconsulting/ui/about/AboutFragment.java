09b0602da59ceccd5f4005e903bb62b7b09447fc
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-03-14 13:37:39 -0100

    Added about page

--- app/src/main/java/com/ratz/atmconsulting/ui/about/AboutFragment.java
+++ app/src/main/java/com/ratz/atmconsulting/ui/about/AboutFragment.java
@@ -11,6 +11,9 @@ import android.view.ViewGroup;
 
 import com.ratz.atmconsulting.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -25,8 +28,35 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+
+        String description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
+                "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n" +
+                "numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n" +
+                "optio, eaque rerum! Provident similique accusantium nemo autem. ";
+
+        Element version = new Element();
+        version.setTitle("Version 1.0");
+
+        return  new AboutPage(getActivity())
+                .setImage(R.drawable.logo)
+                .setDescription(description)
+
+                .addGroup("Contact us")
+                .addEmail("consulting@atm.com", "Let's talk'")
+                .addWebsite("www.google.com", "Check our website")
+
+                .addGroup("Follow us")
+                .addFacebook("consulting", "Facebook")
+                .addInstagram("consulting", "Instagram")
+                .addTwitter("consulting", "Twitter")
+                .addYoutube("consulting", "Youtube")
+                .addGitHub("ratzPereira", "GitHub")
+                .addPlayStore("link", "Check us in your mobile")
+
+                .addItem(version)
+                .create();
+        //return inflater.inflate(R.layout.fragment_about, container, false);
+
     }
 
 }
